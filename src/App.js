ce6838b04e654f18319bf5abd1b475c9707f853e
aha <1360495823@qq.com>
2024-04-10 13:21:33 +0800

    顶部导航栏样式添加

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import NavHeader from './components/NavHeader'
 import PageFooter from './components/PageFooter'
 import { Layout } from 'antd'
+import "./css/App.css"
 const { Header, Footer, Content } = Layout;
 function App() {
   return (
     <div className="App">
-      <Header>
+      <Header className='header'>
         <NavHeader></NavHeader>
       </Header>
-      <Content></Content>
-      <Footer>
+      <Content className='content'></Content>
+      <Footer className='footer'>
         <PageFooter></PageFooter>
       </Footer>
 
--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
+import { Select,Input,Button } from "antd"
 
 function NavHeader(props) {
     return (
-        <div>
-            页面的头部
+        <div className='headerContainer'>
+            {/* logo */}
+            <div className="logoContainer">
+                <div className="logo"></div>
+            </div>
+            {/* 导航 */}
+            <nav className="navContainer">
+                <NavLink to='/' className='navgation'>问答</NavLink>
+                <NavLink to='/books' className='navgation'>书籍</NavLink>
+                <NavLink to='/interviews' className='navgation'>面试题</NavLink>
+                <NavLink to='/' className='navgation'>视频</NavLink>
+                <a href="https://duyi.ke.qq.com/" className='navgation'>视频教程</a>
+            </nav>
+            {/* 搜索框 */}
+            <div className="searchContainer">
+                <Select options={[{ value: 'issue', label: <span>问答</span> }, { value: 'book', label: <span>书籍</span> }]}
+                    defaultValue='issue'
+                    size="large"
+                    style={{width:"20%"}}
+                />
+                <Input.Search placeholder="请输入要搜索的内容" enterButton='搜索' size="large" allowClear style={{width: '80%'}}></Input.Search>
+            </div>
+            <div className="loginBtnContainer">
+                <Button type='primary' size='large'>注册/登录</Button>
+            </div>
+
         </div>
     );
 }
