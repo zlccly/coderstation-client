8445be8bc6821c7b65990b3f72c15fe4f5a18676
aha <1360495823@qq.com>
2024-04-13 13:55:57 +0800

    React-redux + Redux Toolkit安装引入

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import zhCN from 'antd/locale/zh_CN'
 import { ConfigProvider } from 'antd'
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <ConfigProvider locale={zhCN}>
-      <App />
-    </ConfigProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ConfigProvider locale={zhCN}>
+        <App />
+      </ConfigProvider>
+    </BrowserRouter>
+  </Provider>
+
 );
